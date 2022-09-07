29f1a5ccab1622d5c760729f672f27895fbf60fa
Julita <43172762+JulitaNadolska@users.noreply.github.com>
2022-09-07 13:04:06 +0200

    fix tests

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.assertj.core.api.Assertions.assertThat;
-
 class MainTest extends WebDriverSetUp {
 
     @ParameterizedTest
