3932f7f27b15a49477258cd8f1c5d504f5090353
Julita <43172762+JulitaNadolska@users.noreply.github.com>
2022-09-14 13:11:45 +0200

    adding logger

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -4,8 +4,12 @@ import setUp.WebDriverSetUp;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.assertj.core.api.Assertions.assertThat;
 class MainTest extends WebDriverSetUp {
+    private static Logger logger = LoggerFactory.getLogger(MainTest.class);
 
     @ParameterizedTest
     @CsvFileSource(resources = "/data.csv")
@@ -13,6 +17,14 @@ class MainTest extends WebDriverSetUp {
     @DisplayName("Page title test")
     public void TestPageTitle(String pageName, String pageUrl, String expectedTitle) {
         getDriver().get(pageUrl);
+        logger.info("PageUrl is: " + pageUrl);
+        logger.debug("PageUrl is: " + pageUrl);
+        logger.warn("PageUrl is: " + pageUrl);
+        logger.error("PageUrl is: " + pageUrl);
+        logger.trace("PageUrl is: " + pageUrl);
+
+
+
         System.out.println(pageName);
         String actualTitle = getDriver().getTitle();
         assertThat(actualTitle).isEqualTo(expectedTitle);
--- src/test/java/setUp/WebDriverSetUp.java
+++ src/test/java/setUp/WebDriverSetUp.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WebDriverSetUp {
+    private static Logger logger = LoggerFactory.getLogger(WebDriverSetUp.class);
     WebDriver driver;
 
     public WebDriver getDriver() {
@@ -17,16 +20,19 @@ public class WebDriverSetUp {
     @BeforeAll
     static void setupDriver() {
         WebDriverManager.chromedriver().setup();
+        logger.info("Webdriver started successfully");
     }
 
     @BeforeEach
     void startOfProgram() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        logger.info("Browser has been opened");
     }
 
     @AfterEach
     void quitDriver() {
         driver.quit();
+        logger.info("Webdriver closed properly");
     }
 }
